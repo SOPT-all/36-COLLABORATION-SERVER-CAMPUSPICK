f11b69aa5751e593c9c1d4abe99afaed358357e1
kymin <park48664866@gmail.com>
2025-05-14 12:36:18 +0900

    refactor: 미사용 기능 삭제 및 엔드포인트 수정
    미사용 기능 삭제 및 엔드포인트 수정:
    - 사용하지 않는 필드 제거
    - 사용하지 않는 import문 제거
    - 엔트포인트 '/api/v1'추가

--- src/main/java/org/sopt/collaboration/domain/club/repository/ClubRepository.java
+++ src/main/java/org/sopt/collaboration/domain/club/repository/ClubRepository.java
@@ -2,12 +2,11 @@ package org.sopt.collaboration.domain.club.repository;
 
 import org.sopt.collaboration.domain.club.entity.Club;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface ClubRepository extends JpaRepository<Club, Long> {
-    List<Club> findTop5ByOrderByLikeCountDesc();
+	List<Club> findTop5ByOrderByLikeCountDesc();
 }
