78b85f2da91108dc540b2f77c46357c20f1d0674
kymin <park48664866@gmail.com>
2025-05-15 15:46:14 +0900

    fix: 동아리 모집글 검색시 누락된 데이터 추가
    동아리 모집글 검색시 누락된 데이터 추가:
    - 응답 바디에 동아리 카테고리 추가
    - 응답 바디에 동아리 활동 지역 추가
    - 응답 바디에 동아리 활동 요일 추가

--- src/main/java/org/sopt/collaboration/domain/club/dto/ClubSearchResponse.java
+++ src/main/java/org/sopt/collaboration/domain/club/dto/ClubSearchResponse.java
@@ -7,9 +7,15 @@ import org.sopt.collaboration.domain.club.entity.Club;
 import org.sopt.collaboration.domain.club.entity.ClubRecruit;
 
 public record ClubSearchResponse(ClubInfo clubInfo, RecruitPost recruitPost) {
-	public record ClubInfo(long id, String name, String image) {
+	public record ClubInfo(long id, String name, String image, String category, String region, String clubDay) {
 		public ClubInfo(Club club) {
-			this(club.getId(), club.getName(), club.getImage());
+			this(club.getId(),
+				club.getName(),
+				club.getImage(),
+				club.getCategory().getName(),
+				club.getRegion().getRegionName(),
+				club.getClubDay().getName()
+			);
 		}
 	}
 
